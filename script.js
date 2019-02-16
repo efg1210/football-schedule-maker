117813a6175dceb7e183fae20f0f3078f5bb1695
Emily <efg12103@gmail.com>
2019-02-16 14:13:20 -0500

    makes sure that week 10 is not the same as week 1, works until week 13

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ shuffledTeams = shuffledTeams.sort(function(a, b){return 0.5 - Math.random()});
 function start(){
   finalMessage = "";
   var weeks = prompt("Number of weeks?");
+  console.log(shuffledTeams);
   oneShift(weeks);
   if(weeks > (shuffledTeams.length/2)){
     circleShift((weeks - (shuffledTeams.length/2)));
@@ -33,7 +34,6 @@ function oneShift(weeks){
     finalMessage = finalMessage + "<strong>Week number " + (i + 1).toString() + ":</strong> </br>";
     for(var j = 0; j < Math.floor(shuffledTeams.length/2); j++){
       var calculation = (j + i + 9);
-      //console.log(calculation);
       if(calculation <= (shuffledTeams.length - 1)){
         finalMessage = finalMessage + shuffledTeams[j] + " against " + shuffledTeams[calculation] + "</br>";
       }else{
@@ -48,32 +48,32 @@ function circleShift(weeks){
   var arr1 = [];
   var arr2 = [];
   var output = "";
-  for(var i = 0; i < shuffledTeams.length; i+=2){
+  for(var i = 0; i < shuffledTeams.length/2; i++){
     arr1.push(shuffledTeams[i]);
   }
-  for(var i = 1; i < shuffledTeams.length; i+=2){
+  for(var i = shuffledTeams.length/2; i < shuffledTeams.length; i++){
     arr2.push(shuffledTeams[i]);
   }
 
   //creating output message
   for (var i = 0; i < weeksOrHalf(weeks); i++) {
     output += "<b>Week number " + (i+1+(shuffledTeams.length/2)).toString() + "</b><br/>";
-    for (var j = 0; j < arr1.length; j++) {
-      output += arr1[j] + " against " + arr2[j] + "<br/>";
-    }
 
     //preparing for the message
     var temp = arr1[0];
     for(var j = 0; j < ((shuffledTeams.length/2)); j++){
       arr1[j] = arr1[j+1];
-      console.log(j, "first");
     }
     arr1[arr1.length-1] = arr2[arr2.length-1];
     for(var j = (arr2.length - 1); j > 0; j--){
       arr2[j] = arr2[j-1];
-      console.log(j, "second");
     }
     arr2[0] = temp;
+
+    //message
+    for (var j = 0; j < arr1.length; j++) {
+      output += arr1[j] + " against " + arr2[j] + "<br/>";
+    }
   }
   finalMessage += output;
 }
